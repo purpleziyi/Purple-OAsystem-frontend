9f87a4af2ef3a691495457b8f731832a4cd567f7
ziyiz <ziyiz@ulrik.uio.no>
2024-05-09 09:18:04 +0200

    publish complete

--- src/views/inform/publish.vue
+++ src/views/inform/publish.vue
@@ -71,38 +71,43 @@ const onSubmit = () => {
 
 <template>
     <OAMain title="Publish Notification"><!-- 发布通知 -->
+        <el-card>
+            <el-form :model="informForm" :rules="rules" ref="formRef">
+                <el-form-item label="Title" :label-width="formLabelWidth" prop="title"><!-- 标题 -->
+                    <el-input v-model="informForm.title" autocomplete="off" />
+                </el-form-item>
+                <el-form-item label="Departments" :label-width="formLabelWidth" prop="department_ids">
+                    <!--部门可见-->
+                    <el-select multiple v-model="informForm.department_ids"><!--multiple可以实现多选-->
+                        <el-option :value="0"
+                            label="All Departments"></el-option><!-- notification is visible to all departments when value is 0 -->
+                        <el-option v-for="department in departments" :label="department.name" :value="department.id"
+                            :key="department.name" />
+                    </el-select>
+                </el-form-item>
 
-        <el-form :model="informForm" :rules="rules" ref="formRef">
-            <el-form-item label="Title" :label-width="formLabelWidth" prop="title"><!-- 标题 -->
-                <el-input v-model="informForm.title" autocomplete="off" />
-            </el-form-item>
-            <el-form-item label="Departments" :label-width="formLabelWidth" prop="department_ids">
-                <!--部门可见-->
-                <el-select multiple v-model="informForm.department_ids"><!--multiple可以实现多选-->
-                    <el-option :value="0"
-                        label="All Departments"></el-option><!-- notification is visible to all departments when value is 0 -->
-                    <el-option v-for="department in departments" :label="department.name" :value="department.id"
-                        :key="department.name" />
-                </el-select>
-            </el-form-item>
+                <el-form-item label="Content" :label-width="formLabelWidth" prop="content"> <!--内容 富文本框区-->
+                    <div style="border: 1px solid #ccc">
+                        <QuillEditor ref="myQuillEditor" theme="snow" toolbar="full"
+                            v-model:content="informForm.content" class="ql-editor" style="height: 400px" />
+                    </div>
 
-            <el-form-item label="Content" :label-width="formLabelWidth" prop="content"> <!--内容 富文本框区-->
-                <div style="border: 1px solid #ccc">
-                    <QuillEditor ref="myQuillEditor" theme="snow" toolbar="full" v-model:content="informForm.content"
-                        class="ql-editor" style="height: 400px" />
-                </div>
+                    <!-- <div style="border: 1px solid #ccc">
+                        <Toolbar style="border-bottom: 1px solid #ccc" :editor="editorRef" :defaultConfig="toolbarConfig"
+                            :mode="mode" />
+                        <Editor style="height: 500px; overflow-y: hidden;" v-model="informForm.content"
+                            :defaultConfig="editorConfig" :mode="mode" @onCreated="handleCreated" />
+                    </div> -->
+                </el-form-item>
+                <el-form-item>
+                    <div style="text-align: right; flex:1;"><!--提交按钮 置于右下方-->
+                        <el-button type="primary" @click="onSubmit">Submit</el-button>
+                    </div>
+                </el-form-item>
+            </el-form>
 
-                <!-- <div style="border: 1px solid #ccc">
-                    <Toolbar style="border-bottom: 1px solid #ccc" :editor="editorRef" :defaultConfig="toolbarConfig"
-                        :mode="mode" />
-                    <Editor style="height: 500px; overflow-y: hidden;" v-model="informForm.content"
-                        :defaultConfig="editorConfig" :mode="mode" @onCreated="handleCreated" />
-                </div> -->
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="onSubmit">Submit</el-button>
-            </el-form-item>
-        </el-form>
+        </el-card>
+
 
     </OAMain>
 </template>
